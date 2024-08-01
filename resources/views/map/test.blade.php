0516bb0b2fe1d1c556d3b08f2746ed6a700e2084
Zaki Jamali Arafi <zakijamaliarafi@gmail.com>
2024-08-01 15:52:40 +0700

    [feature]  view map: data layer using GeoJSON

--- resources/views/map/test.blade.php
+++ resources/views/map/test.blade.php
@@ -16,25 +16,35 @@
     <script src="{{ asset('js/config.js') }}"></script>
     <script>var apikey = config.MAP_API_KEY;</script>
 
+    <script>
+        function loadGoogleMaps() {
+            var script = document.createElement('script');
+            script.src = `https://maps.googleapis.com/maps/api/js?key=${apikey}&callback=initMap&v=weekly`;
+            script.defer = true;
+            document.head.appendChild(script);
+        }
+
+        loadGoogleMaps();
+    </script>
+
     <!-- prettier-ignore -->
-    <script>(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
-    ({key: apikey, v: "weekly"});</script>
+    {{-- <script>(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
+    ({key: apikey, v: "weekly"});</script> --}}
     
     <script>
-    async function initMap() {
-        const { Map } = await google.maps.importLibrary("maps");
-        const { LatLng } = await google.maps.importLibrary("core");
-        const { TransitLayer } = await google.maps.importLibrary("maps");
+    let map;
 
-        var mapOptions = {
-            zoom: 13,
-            center: new LatLng(51.5, -0.11)
-        }
-        var map = new Map(document.getElementById("map"), mapOptions);
-        var transitLayer = new TransitLayer();
-        transitLayer.setMap(map);
+    function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: { lat: -28, lng: 137 },
+    });
+    
+    var geoJsonUrl = "{{ asset('js/test.geojson') }}";
+    map.data.loadGeoJson(geoJsonUrl);
     }
-    initMap();
+
+    window.initMap = initMap;
     </script>
 
 </x-app-layout>
